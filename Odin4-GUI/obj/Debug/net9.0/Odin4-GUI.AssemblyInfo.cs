7839e367d246d41c42e3af2bca9ff42206c7782e
daglaroglou <christos.daglaroglou@gmail.com>
2025-03-13 19:55:12 +0200

    Update assembly information and binary files for Odin4-GUI

--- Odin4-GUI/obj/Debug/net9.0/Odin4-GUI.AssemblyInfo.cs
+++ Odin4-GUI/obj/Debug/net9.0/Odin4-GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odin4-GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef01f076d79eb1fa1eb0169d4817325a92687ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493dae576f7a521be2f1fbbf2f1e8c1010b8437d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odin4-GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odin4-GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
