73dfaf802c52de3b0972f07d31be2a4234c37924
ApparentlyPlus <panoschatzikallias@gmail.com>
2025-03-16 17:23:41 +0200

    Changed build.sh to publish directly

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # Description: Build Odin4-GUI project
 # Author: daglaroglou
 # Credits: Adrilaw (Odin4), XDA (Odin Tool)
-dotnet build Odin4-GUI.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:Configuration=Release /p:Platform="AnyCPU"
+dotnet publish -p:PublishProfile=LinuxNative
