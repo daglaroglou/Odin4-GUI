a4e8e0a7a295e784ed01c79040b2787744ab9b89
daglaroglou <christos.daglaroglou@gmail.com>
2025-06-28 16:47:33 +0300

    Update release workflow and publish profiles for amd64 and arm64 builds

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # Description: Build Aesir project
 # Author: daglaroglou
 # Credits: Adrilaw (Odin4), XDA (Odin Tool)
-dotnet publish -p:PublishProfile=LinuxNative
+dotnet publish -p:PublishProfile=linux-amd64
+dotnet publish -p:PublishProfile=linux-arm64
